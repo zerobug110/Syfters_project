a7d3fbaff671a3ba503756f189cc8c81b5e1293d
zerobug110 <mahabojang110@gmail.com>
2021-12-18 02:22:15 +0000

    complete

--- product_management/product_management/settings.py
+++ product_management/product_management/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'syfters435@gmail.com'git
+EMAIL_HOST_USER = 'syfters435@gmail.com'
 EMAIL_HOST_PASSWORD = 'xdwegmtvirafeiaj'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
