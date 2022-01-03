d7feca66f4fcf95db70fa48ec95cf484332f44f2
zerobug110 <mahabojang110@gmail.com>
2022-01-03 03:59:57 +0000

    correct the hostname

--- product_management/settings.py
+++ product_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xi&q#%wp(y-uo-^bvj!n)ja*#@vss15t7=b#6#10es5$*p0h^c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['104.131.42.6','www.syfters.com']
+ALLOWED_HOSTS = ['104.131.42.6','www.syfters.info']
 
 
 # Application definition
