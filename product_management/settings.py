e650eee8e3cda60cf4327cf3157a1c50f42b42b6
zerobug110 <mahabojang110@gmail.com>
2022-01-08 15:21:19 +0000

    static mod

--- product_management/settings.py
+++ product_management/settings.py
@@ -139,13 +139,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_root')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_roots')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
