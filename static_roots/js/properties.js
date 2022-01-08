8375e8deaee1c0d25987ae30a98db79deb254674
zerobug110 <mahabojang110@gmail.com>
2022-01-08 15:18:43 +0000

    delete collectstatic

--- static_roots/js/properties.js
+++
@@ -1,9 +0,0 @@
-const navList = document.querySelector('.nav__list');
-const navToggle = document.querySelector('.lines');
-
-navToggle.addEventListener('click', ()=> {
-    navList.classList.toggle('show');    
-});
-
-//generate the current date
-date.innerHTML = new Date().getFullYear();
